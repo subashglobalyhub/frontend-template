7165c1e608fff3d21420bed22526690c2f998f90
subash729 <subaschy729@gmail.com>
2026-09-08 22:25:32 +0545

    inital project

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-  name: "GlobalyOs",
-  description: "GlobalyOs web application.",
+  name: "Iten",
+  description: "Iten web application.",
   url: "https://globalyos.com",
 };
